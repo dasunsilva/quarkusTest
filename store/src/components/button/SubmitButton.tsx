d1a2e40ecf6209152ebb76e11e4e21826ea05c8d
dasunsilva <e19375@eng.pdn.ac.lk>
2024-07-30 10:26:08 +0530

    Working project before messing things up

--- store/src/components/button/SubmitButton.tsx
+++ store/src/components/button/SubmitButton.tsx
@@ -1,28 +1,60 @@
-import { Axios } from "axios";
-import { Button } from "react-bootstrap"
+import { Button } from "react-bootstrap";
+import axiosInstance from "../../axios/Axios";
+import { UserInput } from "../../types/UserInput";
 
-export default function SubmitButton({data, isRemove, isEdit} : {data : <UserInput>, isRemove : boolean, isEdit : boolean}) {
+export default function SubmitButton({
+  data,
+  setData,
+  isRemove,
+  isEdit,
+}: {
+  data: UserInput;
+  setData: React.Dispatch<React.SetStateAction<UserInput>>;
+  isRemove: boolean;
+  isEdit: boolean;
+}) {
+  const submitData = async () => {
+    console.log("Submit data:", data);
 
-    const submitData = () => {
-    const urlBase: string = "http://localhost:8080/users/";
+    try {
+      let response;
 
-    Axios.post(urlBase + "add", {
-      name: data.userName,
-      email: data.userEmail,
-      phone: data.userPhone,
-      accountNumber: data.userAccount,
-    })
-      .then((response) => {
-        console.log(response);
-        // resetData();
-      })
-      .catch((error) => {
-        console.log(error);
+      if (isRemove) {
+        response = await axiosInstance.delete(`/users/remove/${data.userId}`);
+      } else if (isEdit) {
+        response = await axiosInstance.put(`/users/edit/${data.userId}`, {
+          id: data.userId,
+          name: data.userName,
+          email: data.userEmail,
+          phone: data.userPhone,
+          accountNumber: data.userAccount,
+        });
+      } else {
+        console.log(data);
+        response = await axiosInstance.post("/users/add", {
+          name: data.userName,
+          email: data.userEmail,
+          phone: data.userPhone,
+          accountNumber: data.userAccount,
+        });
+      }
+
+      console.log(response);
+      setData({
+        userId: "",
+        userName: "",
+        userEmail: "",
+        userPhone: "",
+        userAccount: "",
       });
+    } catch (error) {
+      console.error("Error occurred while submitting data:", error);
+    }
   };
-    return (
-      <Button variant="outline-primary" id="btnPrimary" onClick={submitData}>
-          Submit
-        </Button>{" "}
+
+  return (
+    <Button variant="outline-primary" id="btnPrimary" onClick={submitData}>
+      Submit
+    </Button>
   );
 }
