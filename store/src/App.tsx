18b6a324c94682093e6403f709104d322a30531e
dasunsilva <e19375@eng.pdn.ac.lk>
2024-07-30 17:14:04 +0530

    Add routes

--- store/src/App.tsx
+++ store/src/App.tsx
@@ -3,30 +3,34 @@ import "./assets/css/App.css";
 import MainNavBar from "./components/navBar/MainNavBar";
 import UserDataTable from "./components/user/UserDataTable";
 import AddNewUser from "./components/user/UserOperations";
+import { ReactKeycloakProvider } from "@react-keycloak/web";
+import keycloak from "./services/keycloak";
 
 function App() {
   return (
     <main className="main">
-      <Router>
-        <MainNavBar />
-        <Routes>
-          <Route path="/users">
-            <Route index path="/users/get" element={<UserDataTable />} />
-            <Route
-              path="/users/add"
-              element={<AddNewUser edit={false} remove={false} />}
-            />
-            <Route
-              path="/users/edit"
-              element={<AddNewUser edit={true} remove={false} />}
-            />
-            <Route
-              path="/users/remove"
-              element={<AddNewUser edit={false} remove={true} />}
-            />
-          </Route>
-        </Routes>
-      </Router>
+      <ReactKeycloakProvider authClient={keycloak}>
+        <Router>
+          <MainNavBar />
+          <Routes>
+            <Route path="/users">
+              <Route index path="/users/get" element={<UserDataTable />} />
+              <Route
+                path="/users/add"
+                element={<AddNewUser edit={false} remove={false} />}
+              />
+              <Route
+                path="/users/edit"
+                element={<AddNewUser edit={true} remove={false} />}
+              />
+              <Route
+                path="/users/remove"
+                element={<AddNewUser edit={false} remove={true} />}
+              />
+            </Route>
+          </Routes>
+        </Router>
+      </ReactKeycloakProvider>
     </main>
   );
 }
--- store/src/services/keycloak.tsx
+++ store/src/services/keycloak.tsx
@@ -1,7 +1,7 @@
 import Keycloak from "keycloak-js";
 
 const keycloak = new Keycloak({
-  url: "http://localhost:8082",
+  url: "http://localhost:8080/auth",
   realm: "new-realm",
   clientId: "front-end-client",
 });
