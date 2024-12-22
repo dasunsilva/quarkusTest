d77110c1cf0a01be668f4173f07d6db7845d8eb9
Dasun Udayanga <115541700+dasunsilva@users.noreply.github.com>
2024-12-22 14:53:24 +0530

    Delete BackendReactive directory

--- BackendReactive/src/main/java/org/dasun/repo/BillItemRepo.java
+++
@@ -1,8 +0,0 @@
-package org.dasun.repo;
-
-/**
- * BillItem repo make bill item related database transactions.
- */
-//@ApplicationScoped
-//public class BillItemRepo implements PanacheRepository<BillItems> {
-//}
